c7529bd9b53cb7102975313d675e67b1a6e38f5f
kimthanun <thanunkim168@gmail.com>
2019-04-30 13:53:53 +0700

    modify login form

--- Homework/homwork_2019_04_27/index.php
+++ Homework/homwork_2019_04_27/index.php
@@ -63,7 +63,7 @@
                     </div>
 
                     <div class="p-2 mt-5">
-                        <nav id="nav-bar-menu" class="navbar navbar-expand-lg navbar-light bg-light pl-5">
+                        <nav id="nav-bar-menu" class="navbar navbar-expand-lg navbar-light bg-light">
                             <a class="navbar-brand" href="#"></a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
